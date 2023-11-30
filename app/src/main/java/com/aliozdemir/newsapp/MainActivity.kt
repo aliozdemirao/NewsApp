26e2f8984803a45407cbdc1c508746a40a2267a8
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2023-11-30 23:49:59 +0300

    OnBoardingScreens

--- app/src/main/java/com/aliozdemir/newsapp/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/newsapp/MainActivity.kt
@@ -3,23 +3,29 @@ package com.aliozdemir.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.aliozdemir.newsapp.ui.theme.NewsAppTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.core.view.WindowCompat
+import com.aliozdemir.newsapp.presentation.onboarding.OnBoardingScreen
 
+//TODO: Use the accompanist library to change the colors of the status bar
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         installSplashScreen()
         setContent {
-            NewsAppTheme {
-
+            NewsAppTheme(
+                dynamicColor = false
+            ) {
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
