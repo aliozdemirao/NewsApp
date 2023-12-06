77f1945cb0bd57fc47ab2770a401a86173fd9362
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2023-12-06 23:33:16 +0300

    NavGraph

--- app/src/main/java/com/aliozdemir/newsapp/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/newsapp/presentation/mainActivity/MainActivity.kt
@@ -1,9 +1,9 @@
-package com.aliozdemir.newsapp
+package com.aliozdemir.newsapp.presentation.mainActivity
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -11,29 +11,25 @@ import com.aliozdemir.newsapp.ui.theme.NewsAppTheme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.aliozdemir.newsapp.domain.usecases.app_entry.AppEntryUseCases
-import com.aliozdemir.newsapp.presentation.onboarding.OnBoardingScreen
-import com.aliozdemir.newsapp.presentation.onboarding.OnBoardingViewModel
+import com.aliozdemir.newsapp.presentation.navgraph.NavGraph
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition(condition = { viewModel.splashCondition.value })
+        }
         setContent {
             NewsAppTheme(dynamicColor = false) {
                 Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(onEvent = viewModel::onEvent)
+                    NavGraph(startDestination = viewModel.startDestination.value)
                 }
             }
         }
